715b2d64fd1182c19d4b6c091c6586af15ca9082
LeandroYasutake <leandroyasutake@gmail.com>
2023-05-18 20:21:05 -0300

    terminando logica, testeando. falta borrar

--- js/tickets.js
+++ js/tickets.js
@@ -1,71 +1,70 @@
-var selectElement = document.getElementById('inputCategoria');
-selectElement.addEventListener('change', function() {
-    var selectedValue = this.value;
-    alert(selectedValue);
-  });
+var form = document.getElementById("myForm");
+var nombre = document.getElementById("inputNombre");
+var apellido = document.getElementById("inputApellido");
+var correo = document.getElementById("inputCorreo");
+var inputCantidad = document.getElementById('inputCantidad');
+var inputCategoria = document.getElementById('inputCategoria');
+var totalApagar = document.getElementById('totalApagar');
+const btnClick = document.getElementById('compra-btn');
+btnClick.addEventListener("click",validarFormulario);
 
-  var form = document.getElementById("myForm");
-  var nombre = document.getElementById("inputNombre");
-  var apellido = document.getElementById("inputApellido");
-  var correo = document.getElementById("inputCorreo");
+function validarFormulario(){
 
-  var inputCantidad = document.getElementById('inputCantidad');
-  var inputCategoria = document.getElementById('inputCategoria');
-  var totalApagar = document.getElementById('totalApagar');
-
-  function validarFormulario(){
-
-    if (contieneNumeros(nombre)) {
-      alert("Nombre no debe contener números");
-      nombre.focus();
-      return false;
-    } else if (contieneNumeros(apellido)) {
-      alert("Apellido no deben contener números");
-      apellido.focus();
-      return false;
-    }
-
-    if (!esCorreoValido(correo.value)) {
-      alert("Ingrese una dirección de correo válida");
-      correo.focus();
-      return false;
-    }
-
-    if (inputCantidad < 1) {
-      alert("Ingrese una cantidad de tickets válida");
-      inputCantidad.focus();
-      return false;
-    }
-
-    actualizarMontoTotal();
-    return true;
+  if (contieneNumeros(nombre)) {
+    alert("Nombre no debe contener números");
+    nombre.focus();
+    return false;
+  } else if (contieneNumeros(apellido)) {
+    alert("Apellido no deben contener números");
+    apellido.focus();
+    return false;
   }
 
-  function esCorreoValido(email) {
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  if (!esCorreoValido(correo.value)) {
+    alert("Ingrese una dirección de correo válida");
+    correo.focus();
+    return false;
   }
 
-  function contieneNumeros(input) {
-    var numberRegex = /\d/;
-    return numberRegex.test(input);
+  if (!esCantidadValida(inputCantidad.value)) {
+    alert("Ingrese una cantidad de tickets válida");
+    inputCantidad.focus();
+    return false;
   }
 
-  function actualizarMontoTotal() {
-    var cantidad = inputCantidad.value;
-    var categoria = inputCategoria.value;
-    var precioUnitario = 200;
-    var descuento = 0;
+  actualizarMontoTotal();
+  return true;
+}
 
-    if (categoria === 'estudiante') {
-      descuento = 0.2; 
-    } else if (categoria === 'trainee') {
-      descuento = 0.5; 
-    } else {
-      descuento = 0.75;
-    }
+function esCorreoValido(email) {
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
 
-    var total = precioUnitario * descuento * cantidad;
-    totalApagar.textContent = 'Total a Pagar $: ' + total;
+function contieneNumeros(input) {
+  var numberRegex = /\d/;
+  return numberRegex.test(input);
+}
+
+function esCantidadValida(cantidad) {
+  var cantidadRegex = /^[1-9]\d*$/;
+  return cantidadRegex.test(cantidad);
+}
+
+function actualizarMontoTotal() {
+  var cantidad = inputCantidad.value;
+  var categoria = inputCategoria.value;
+  var precioUnitario = 200;
+  var descuento = 0;
+
+  if (categoria === 'estudiante') {
+    descuento = 0.2; 
+  } else if (categoria === 'trainee') {
+    descuento = 0.5; 
+  } else {
+    descuento = 0.85;
   }
-  
+
+  var total = precioUnitario * descuento * cantidad;
+  totalApagar.textContent = 'Total a Pagar $: ' + total;
+}
